e3f58348c9c4b02270b27c23430c79cf37ae60c8
Anna Battistals <anna.r.battista@gmail.com>
2020-10-21 17:35:11 -0600

    added Employee.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,36 +1,33 @@
 // TODO: Write code to define and export the Employee class
-//need inquirer
-const inquirer =require("inquirer");
-const util =require("util");
 
 //Add employee
-const employee = [
-  {
-    //add id number
-    type:"input",
-    message:"What is the employee ID?",
-    name:"eId",
-  },
-  {
-    //add email
-    type:"input",
-    message:"What is the employee's e-mail address?",
-    name:"email",
-  },
-  {
-    //ask employee type (engineer/intern), use to decide which file to go to to change class...maybe?
-    type:"list",
-    message:"What is the type of employee?",
-    name:"eType",
-    choices: ["engineer", "intern", "manager"]
-  },
-
-   
-]
+class Employee {
+  constructor(name, id, email){
+  this.name = name;
+  //add id number
+  this.id = id
+  //add email
+  this.email =email;
+  //ask employee type (engineer/intern/manager), use to decide which file to go to to change class...maybe?
+  // this. role = role;
+};
 
 //get employee information
-inquirer.prompt(employee)
-// console.log (employee.data)
+getName(){
+  return this.name
+};
+getId(){
+  return this.id
+};
+getEmail(){
+  return this.email
+};
 //display employee as role
-  getRole(employee)
+getRole(){
+return "employee"};
+}
  //Add to main html page
+
+ const worker = new Employee("anna", "anna@email.com","29",getRole);
+ console.log (worker)
+
